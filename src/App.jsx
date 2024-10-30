7330d45e5075cf07298c53edde9ea5fc6de078e6
AndresTabor <andrestaborda43@gmail.com>
2024-10-30 17:34:28 +0100

    implement todoReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,38 @@
-import { useEffect, useReducer, useState } from 'react'
+import { useReducer } from 'react';
 
-import './App.css'
-import { TodoList } from './components/TodoList'
-import { FormAddTodo } from './components/FormAddTodo'
-import { v4 as uuidv4 } from 'uuid';
+
+import './App.css';
 import { Footer } from './components/Footer';
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
-import { localStorageReducer } from './reducer/localStorageReducer';
+import { FormAddTodo } from './components/FormAddTodo';
+import { TodoList } from './components/TodoList';
+import { todoReducer } from './reducer/todosReducer';
+
+const initialTodos = () => {
+  return JSON.parse(localStorage.getItem("todos")) || []
+}
 
 function App() {
-  const [todos, setTodos] = useState([])
-  const [state, dispatch] = useReducer(localStorageReducer, todos)
+  const [state, dispatch] = useReducer(todoReducer,null,initialTodos)
   
-
-  const showSwal = () => {
-    withReactContent(Swal).fire({
-      icon: "error",
-      title: <i>Something wrong!</i>,
-      text: 'Task already exists',
-      
-    })
-  }
   
    const handleAddTodo = (title) => {
-    const wasAdded = todos.filter(task => task.title.toUpperCase() === title.toUpperCase())
-    if (wasAdded.length > 0) {
-      showSwal()
-      return
-    }
-    const newTodo = {
-      id: uuidv4(),
-      title: title,
-      isSaved: false
-    }
-    setTodos(prevTodos => [...prevTodos, newTodo])
+    dispatch({type: "add_todo", payload: { title: title }})
+    
   }
 
-  const handleRemoveTodo = (id) => {
-    const updateTodoList = todos.filter(todo => todo.id !== id)
-    setTodos(updateTodoList)
+  const handleRemoveTodo = (id) => {    
+    dispatch({type: "unblock_todo", payload: { id: id }})
+    dispatch({type: "delete_todo", payload: { id: id }})
   }
   
   const handleSavedStorageTodo = (id) => {
-    dispatch({type: "save_todo", id: id, todos: todos})
-    
-    //setTodos(state)
+    dispatch({type: "block_todo", payload: { id: id }})         
   }
 
   const handleDeleteStorageTodo = (id) => {    
-    dispatch({type: "remove_todo", id: id, todos: todos})
-    // const listTodos = [...todos]
-    // const todoToSaved = listTodos.find(todo => todo.id === id)
-
-    // if (todoToSaved) {
-    //   todoToSaved.isSaved = false
-    //   setTodos(listTodos)
-    //   localStorage.setItem("todos", JSON.stringify(
-    //     listTodos.filter(todo => todo.isSaved === true)
-    //   ))      
-    // }
+    dispatch({type: "unblock_todo", payload: { id: id }})
   }
 
-  useEffect(() => {
-    const savedTodos = JSON.parse(localStorage.getItem("todos")) || []
-    setTodos(savedTodos)
-    
-  }, []);
 
   return (
     <section className='todoapp'>
@@ -75,9 +41,9 @@ function App() {
         <FormAddTodo createTodo={handleAddTodo}/>     
       </header>
       {
-        todos.length > 0 ? <TodoList todos={todos} removeTodos={handleRemoveTodo} lockTodo={handleSavedStorageTodo} unLockTodo={handleDeleteStorageTodo}/> : <p>No hay tareas, añadir tareas</p>
+        state.length > 0 ? <TodoList todos={state} removeTodos={handleRemoveTodo} lockTodo={handleSavedStorageTodo} unLockTodo={handleDeleteStorageTodo}/> : <p>No hay tareas, añadir tareas</p>
       }
-      <Footer todoItems={todos.length}/>      
+      <Footer todoItems={state.length}/>      
     </section>
   )
 }
---
+++ src/reducer/todosReducer.js
@@ -0,0 +1,78 @@
+
+import { v4 as uuidv4 } from 'uuid';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+// const showSwal = () => {
+//   withReactContent(Swal).fire({
+//     icon: "error",
+//     title: <i>Something wrong!</i>,
+//     text: 'Task already exists'
+    
+//   })
+// }
+
+
+export const todoReducer = (state, action) => {
+  
+
+  const listTodos = [...state]
+  
+
+  const updateStorage = ( listTodos ) => {
+
+    localStorage.setItem("todos", JSON.stringify(
+      listTodos.filter(todo => todo.isSaved === true)
+    ))
+    return listTodos
+  }
+
+  switch (action.type) {
+    case "add_todo":
+      {
+        const wasAdded = listTodos.filter(task => task.title.toUpperCase() === action.payload.title.toUpperCase())
+
+        if (wasAdded.length > 0) {
+          alert("Task already exists")
+          return state
+        }
+        const newTodo = {
+          id: uuidv4(),
+          title: action.payload.title,
+          isSaved: false
+        }
+        return [...state, newTodo]
+      }
+    case "delete_todo":
+      {
+        const updateTodoList = state.filter(todo => todo.id !== action.payload.id)
+        return updateTodoList
+      }
+    case "block_todo":
+      {       
+        
+        const todoToSaved = listTodos.find(todo => todo.id === action.payload.id)
+        if (todoToSaved) {
+          todoToSaved.isSaved = true                     
+          updateStorage( listTodos )
+          return listTodos 
+        }
+        return state
+      }
+    case "unblock_todo":
+      {       
+        const todoToSaved = listTodos.find(todo => todo.id === action.payload.id)
+
+        if (todoToSaved) {
+          todoToSaved.isSaved = false                     
+          updateStorage( listTodos )
+          return listTodos 
+        }
+        return state
+      }
+    
+    default:
+      throw Error('Unknown action: ' + action.type);
+    
+  }
+}
